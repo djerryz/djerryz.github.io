f0476351366deaeafdce30d2721777340d2581b1
jerrywannafly <hihisir1amsexyman@gmail.com>
2017-08-08 01:43:10 +0800

    .

---
+++ public/js/default.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+	var a;
+	var b = 0;
+	var checkonnews = {
+		"opacity": "1",
+		"transform": "translate3d(0, 0.32rem, 0)",
+		"box-shadow": "0 0 0 1px rgba(0, 0, 0, 0.15)",
+		"z-index": "40",
+		"display": "block"
+	};
+	var checkoutnews = {
+		"opacity": "0",
+		"transform": "translate3d(0, -0.32rem, 0)",
+		"box-shadow": "none",
+		"z-index": "-5"
+	};
+	var checkonhas = {
+		"opacity": "1"
+	};
+	var checkouthas = {
+		"opacity": "0.2"
+	};
+	$(".has-ajax,.news").mouseover(function () {
+		if (b == 0) {
+			$(".news").css(checkonnews);
+			$(".has-ajax").css(checkonhas);
+			b = 1;
+		} else {
+			clearTimeout(a);
+		}
+	});
+	$(".has-ajax,.news").mouseout(function () {
+		a = setTimeout(function () {
+				$(".news").css(checkoutnews);
+				$(".has-ajax").css(checkouthas);
+				b = 0;
+			}, 340);
+	});
+	$(".close").click(function () {
+		$(".news").css(checkoutnews);
+		$(".has-ajax").css(checkouthas);
+		b = 0;
+	});
+	myToggle = document.querySelector('.toggle');
+	myToggle.addEventListener('click', function () {
+		if (myToggle.classList.contains('open')) {
+			myToggle.classList.remove('open');
+		} else {
+			myToggle.classList.add('open');
+		}
+	});
+});
